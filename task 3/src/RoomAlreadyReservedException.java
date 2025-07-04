1d299c1655c953df5fca0a4c9d7f79990a31b2be
Rasim <120395587+Fenixail@users.noreply.github.com>
2025-07-04 10:27:00 +0400

    Delete task 3/src directory

--- task 3/src/RoomAlreadyReservedException.java
+++
@@ -1,6 +0,0 @@
-// RoomAlreadyReservedException.java
-public class RoomAlreadyReservedException extends RuntimeException {
-    public RoomAlreadyReservedException(String message) {
-        super(message);
-    }
-}
